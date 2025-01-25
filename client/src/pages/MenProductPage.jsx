85ff8910a690a12c273bbb3222cbb8b41cf06f9d
RyomenDev <aakashvatsh007@gmail.com>
2025-01-25 11:49:18 +0530

    loadProducts

--- client/src/pages/MenProductPage.jsx
+++ client/src/pages/MenProductPage.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import Skeleton from "../components/card/Skeleton";
 import PremiumCard from "../components/card/PremiumCard";
 
-const baseUrl = import.meta.env.VITE_BASE_URL; // `${baseUrl}`
+import { fetchProductsByCategory } from "../../api/Pages/PagesApi";
 
 const MenProductPage = () => {
   const { productName } = useParams();
@@ -13,66 +13,64 @@ const MenProductPage = () => {
   const [loading, setLoading] = useState(false);
   const [totalProducts, setTotalProducts] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
- 
 
   useEffect(() => {
-    const fetchProductsByCategory = async () => {
+    const loadProducts = async () => {
       setLoading(true);
       try {
-        const response = await fetch(`${baseUrl}/api/product/getAllProducts?categoryName=${categoryName}&page=${currentPage}`);
-        if (!response.ok) {
-          throw new Error('Failed to fetch products');
-        }
-        const data = await response.json();
+        const data = await fetchProductsByCategory(categoryName, currentPage);
         setProducts(data.products);
-        setLoading(false);
         setTotalProducts(data.totalProducts);
+        setLoading(false);
       } catch (error) {
-        console.error('Error fetching products:', error);
+        console.error("Error fetching products:", error);
+      } finally {
         setLoading(false);
       }
     };
 
-    fetchProductsByCategory();
+    loadProducts();
   }, [categoryName, currentPage]);
 
-  const totalPages = Math.ceil(totalProducts/9);
+  const totalPages = Math.ceil(totalProducts / 9);
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
 
   return (
-  
     <div className="py-20 dark:bg-matteBlack flex flex-col justify-center items-center">
-    <h1 className="text-center text-xl font-semibold py-8">{categoryName}</h1>
-    {loading ? ( // Render skeleton if loading is true
-      <div>
-        <Skeleton />
-        <Skeleton />
-        <Skeleton />
+      <h1 className="text-center text-xl font-semibold py-8">{categoryName}</h1>
+      {loading ? ( // Render skeleton if loading is true
+        <div>
+          <Skeleton />
+          <Skeleton />
+          <Skeleton />
+        </div>
+      ) : (
+        // Render product details or content when loading is false
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-7">
+          {products.map((product) => (
+            <PremiumCard key={product._id} product={product} />
+          ))}
+        </div>
+      )}
+      <div className="flex justify-center mt-8">
+        {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+          (page) => (
+            <button
+              key={page}
+              className={`mx-1 px-4 py-2 bg-blue-500 text-white rounded ${
+                currentPage === page ? "bg-blue-600" : ""
+              }`}
+              onClick={() => handlePageChange(page)}
+            >
+              {page}
+            </button>
+          )
+        )}
       </div>
-    ) : (
-      // Render product details or content when loading is false
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-7">
-        {products.map((product) => (
-              <PremiumCard key={product._id} product={product} />
-        ))}
-      </div>
-    )}
-    <div className="flex justify-center mt-8">
-        {Array.from({ length: totalPages }, (_, index) => index + 1).map((page) => (
-          <button
-            key={page}
-            className={`mx-1 px-4 py-2 bg-blue-500 text-white rounded ${currentPage === page ? 'bg-blue-600' : ''}`}
-            onClick={() => handlePageChange(page)}
-          >
-            {page}
-          </button>
-        ))}
-      </div>
-  </div>
-    
+    </div>
   );
 };
 
