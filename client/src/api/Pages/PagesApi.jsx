236427e70821b8e977b7b0456d10be24566c689f
RyomenDev <aakashvatsh007@gmail.com>
2025-01-25 11:41:58 +0530

    logoutApi

--- client/src/api/Pages/PagesApi.jsx
+++ client/src/api/Pages/PagesApi.jsx
@@ -6,10 +6,33 @@ const baseUrl = conf.SERVER_API_URL;
 // Fetch products
 export const fetchProducts = async (searchQuery) => {
   try {
-    const res = await axios.get(`${baseUrl}/api/product/getProducts?${searchQuery}`);
+    const res = await axios.get(
+      `${baseUrl}/api/product/getProducts?${searchQuery}`
+    );
     return res.data; // Return the API response
   } catch (error) {
     console.error("Error fetching products:", error);
     throw error; // Throw the error to handle it in the component
   }
 };
+
+
+// API call to log out the user
+export const logoutUser = async (accessToken) => {
+  try {
+    const response = await axios.post(
+      `${baseUrl}/api/users/logout`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`, // Include the access token
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error logging out:", error);
+    throw new Error(error.response?.data?.message || "Logout failed");
+  }
+};
+
